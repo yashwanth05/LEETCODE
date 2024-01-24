546e01a220d3c6dfec4a6c26f2a71a56d5398311
SAI YASHWANTH <106529156+yashwanth05@users.noreply.github.com>
2024-01-24 06:14:43 +0000

    Sync LeetCode submission - Concatenation of Array (python)

---
+++ problems/concatenation_of_array/solution.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        # arr = [0]*(2*len(nums))
+        # for i in range(0,len(nums)):
+        #     arr[i] = nums[i]
+        #     arr[i+len(nums)] = nums[i]
+        # return arr
+        # nums+=nums
+        return nums+nums
+
+        
