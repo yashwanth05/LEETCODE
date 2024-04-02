51d2c811cdd96e0c80c60785c479236450d49c4d
SAI YASHWANTH <106529156+yashwanth05@users.noreply.github.com>
2024-04-02 04:54:56 +0000

    Sync LeetCode submission - Length of Last Word (cpp)

---
+++ problems/length_of_last_word/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i = s.size()-1;
+        int ans = 0;
+        
+            while(ans==0 && s[i]==' '){
+                i--;
+            }
+ 
+            while(i>=0 && s[i]!=' '){
+                ans++;
+                i--;
+                
+            }
+        
+        return ans;
+    }
+};
