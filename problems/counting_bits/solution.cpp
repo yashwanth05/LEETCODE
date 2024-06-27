3996540f0216522de702f81c7d7c2cd8b5efae47
SAI YASHWANTH <106529156+yashwanth05@users.noreply.github.com>
2024-06-27 15:48:27 +0000

    Sync LeetCode submission - Counting Bits (cpp)

---
+++ problems/counting_bits/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans;
+        for(int i=0;i<=n;i++){
+            int temp = i;
+            int count = 0;
+            while(temp!=0){
+                if(temp%2==1){
+                    count++;
+                }
+                temp = temp>>1;
+            }
+            cout<<i;
+            ans.push_back(count);
+        }
+        return ans;
+    }
+};
