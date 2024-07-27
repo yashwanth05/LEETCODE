e3eb927be07cd84c8f25e45e6a39d0f883ba6d20
SAI YASHWANTH <106529156+yashwanth05@users.noreply.github.com>
2024-07-27 07:13:22 +0000

    Sync LeetCode submission - Min Stack (cpp)

---
+++ problems/min_stack/solution.cpp
@@ -0,0 +1,36 @@
+class MinStack {
+public:
+    vector<vector<int>> st;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        int min_val = getMin();
+        if(st.empty()||min_val>val){
+            min_val = val;
+        }
+        st.push_back({val,min_val});
+    }
+    
+    void pop() {
+        st.pop_back();
+    }
+    
+    int top() {
+        return st.empty() ? -1 : st.back()[0];
+    }
+    
+    int getMin() {
+        return st.empty() ? -1 :st.back()[1];
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
