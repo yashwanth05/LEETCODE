98f3ad5a3b1f16d3196c9f03db1d2815cad8c891
SAI YASHWANTH <106529156+yashwanth05@users.noreply.github.com>
2024-09-19 13:15:08 +0000

    Sync LeetCode submission - Sort Colors (cpp)

--- problems/sort_colors/solution.cpp
+++ problems/sort_colors/solution.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        int len = nums.size();
+        for(int i=0;i<len;i++){
+            if(nums[i] == 0) nums.push_back(nums[i]);
+        }
+        for(int i=0;i<len;i++){
+            if(nums[i] == 1) nums.push_back(nums[i]);
+        }
+        for(int i=0;i<len;i++){
+            if(nums[i] == 2) nums.push_back(nums[i]);
+        }
+        for(int i=0;i<len;i++){
+            nums.erase(nums.begin());
+        }
     }
 };
